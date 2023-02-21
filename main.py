1625aa3002b895a3e4aa605822ba83458a1aa145
serendipityyyy <emoji.princ3ss@gmail.com>
2023-02-20 21:00:38 -0600

    add toggle power example code

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 print('hello world')
+
+import requests
+
+token = "cad51ac2161cb093ec6226896055acd362f92e39fb97da8f5b4db8efac490e22"
+
+headers = {
+    "Authorization": "Bearer %s" % token,
+}
+
+response = requests.post('https://api.lifx.com/v1/lights/all/toggle', headers=headers)
