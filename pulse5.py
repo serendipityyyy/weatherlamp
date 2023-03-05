beac167fe403be2d42cca766b95cf8896c3d5f2d
serendipityyyy <emoji.princ3ss@gmail.com>
2023-03-04 22:37:49 -0600

    5th attempt pulse effect

---
+++ pulse5.py
@@ -0,0 +1,15 @@
+import requests
+
+token = "cad51ac2161cb093ec6226896055acd362f92e39fb97da8f5b4db8efac490e22"
+
+headers = {
+    "Authorization": "Bearer %s" % token,
+}
+
+data = {
+    "period": 2,
+    "cycles": 5,
+    "color": "green",
+}
+
+response = requests.post('https://api.lifx.com/v1/lights/all/effects/pulse', data=data, headers=headers)
