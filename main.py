d617588fbb86d02874d24f182a26bb2fe13969fc
serendipityyyy <emoji.princ3ss@gmail.com>
2023-04-05 18:57:29 -0500

    new weather functions

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ from lights import set_lights_to_purple
 from lights import set_lights_to_teal
 from weather import get_temperature
 from weather import get_percent_of_precipitation
+
+
 config = configparser.ConfigParser()
 config.read('config.ini')
 TOKEN = config['DEFAULT']['LIFXAPIkey']
 WEATHER_STATION = config['WEATHER']['Station']
 WEATHER_LATITUDE = config['WEATHER']["Latitude"]
 WEATHER_LONGITUDE = config['WEATHER']['Longitude']
+
 
 def set_color_from_weather_condition(temperature, precipitation):
     if precipitation > 0:
@@ -26,16 +29,9 @@ def set_color_from_weather_condition(temperature, precipitation):
             set_lights_to_purple(TOKEN)
 
 
-
-
-
 temperature = get_temperature(WEATHER_STATION, WEATHER_LATITUDE, WEATHER_LONGITUDE)
-precipitation = get_percent_of_precipitation()
+precipitation = get_percent_of_precipitation(WEATHER_STATION, WEATHER_LATITUDE, WEATHER_LONGITUDE)
 print(f"The temperature is: {temperature}, and the precipitation chance is {precipitation}")
 set_color_from_weather_condition(temperature, precipitation)
 time.sleep(5)
-set_lights_to_off()
-
-
-
-
+set_lights_to_off(TOKEN)
